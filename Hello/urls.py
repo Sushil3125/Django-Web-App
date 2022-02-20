e39427f5668e1163b087d1d37c4de08ac2c90d0e
sushilld <sushilldhakal25@gmail.com>
2022-02-20 21:50:13 +0545

    Deploy

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 admin.site.site_header = "Sushil Dhakal's Admin"
 admin.site.site_title = "Sushil Dhakal's Portal"
 admin.site.index_title = "Welcome to Sushil Researcher Portal"
@@ -25,5 +28,8 @@ admin.site.index_title = "Welcome to Sushil Researcher Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls'))
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
  
